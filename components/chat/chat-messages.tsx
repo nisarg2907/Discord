bcdef14df07ca8cbfe53a87effbcc956685958fb
Nisarg <nisargshah.ict19@gmail.com>
2024-01-04 17:53:36 +0530

    fixed the error while fetching messages from api/messages

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -65,8 +65,13 @@ export const ChatMessages = ({
      
     return ( <div className="flex-1 flex flex-col py-4 overflow-y-auto">
             <div className="flex-1" />
-           <ChatWelcome type={type} name={name}/>
-              
+            {!hasNextPage && <div className="flex-1" />}
+      {!hasNextPage && (
+        <ChatWelcome
+          type={type}
+          name={name}
+        />
+      )}
         
          </div> );
 }
