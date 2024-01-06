70b2068d5e35c57b6945a15513ff064a427c14ed
Nisarg Shah <nisargshah.ict19@gmail.com>
2024-01-06 14:25:42 +0530

    Update layout.tsx ts error 2

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -8,7 +8,7 @@ const MainLayout = async ({
   return ( 
     <div className="h-full">
       <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
-         {/* @ts-expect-error Async Server Component*/}
+        
         <NavigationSidebar />
       
       </div>
